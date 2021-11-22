3750c97037fd6968256a950d0108ea7377a77cc8
vishnupv074 <vishnupv074@gmail.com>
2021-11-22 10:32:24 +0530

    Class based views CRUD+L

--- leads/urls.py
+++ leads/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import (lead_list, lead_detail, lead_create, lead_update, lead_delete,
-    LeadListView,
+    LeadListView, LeadDetailView, LeadCreateView, LeadUpdateview, LeadDeleteView
 )
 
 app_name = 'leads'
 
 urlpatterns = [
     path('', LeadListView.as_view(), name='leads_list'),
-    path('<int:pk>/', lead_detail, name='leads_details'),
-    path('create/', lead_create, name='create_lead'),
-    path('<int:pk>/update/', lead_update, name='update_lead'),
-    path('<int:pk>/delete/', lead_delete, name='delete_lead'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='leads_details'),
+    path('create/', LeadCreateView.as_view(), name='create_lead'),
+    path('<int:pk>/update/', LeadUpdateview.as_view(), name='update_lead'),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='delete_lead'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.http import HttpResponse
 from .models import Agent, Lead
 from .forms import LeadForm, LeadModelForm
 from django.views.generic import (
     TemplateView,
     ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
 )
+from django.urls import reverse_lazy
 
 #####
 class LandingPageView(TemplateView):
@@ -19,6 +24,7 @@ def landing_page(request):
 #####
 class LeadListView(ListView):
     model = Lead
+    # queryset = Lead.objects.all()
     context_object_name = 'leads'
     template_name = 'leads/leads_list.html'
 
@@ -31,11 +37,19 @@ def lead_list(request):
 #####
 
 #####
+class LeadDetailView(DetailView):
+    template_name = 'leads/leads_details.html'
+    # queryset = Lead.objects.all()
+    model = Lead
+    context_object_name = 'lead'
+
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     context = {'lead': lead}
     return render(request, 'leads/leads_details.html', context)
+#####
 
+#####
 
 """
 def lead_create(request):
@@ -59,6 +73,14 @@ def lead_create(request):
     return render(request, 'leads/lead_create.html', context)
 """
 
+class LeadCreateView(CreateView):
+    template_name = 'leads/lead_create.html'
+    form_class = LeadModelForm
+    success_url = reverse_lazy('leads:leads_list')
+
+    # def get_success_url(self) -> str:
+    #     return reverse('leads:leads_list')
+
 def lead_create(request):
     form = LeadModelForm()
     if request.method == 'POST':
@@ -71,6 +93,16 @@ def lead_create(request):
     }
     return render(request, 'leads/lead_create.html', context)
 
+#####
+
+#####
+class LeadUpdateview(UpdateView):
+    template_name = 'leads/leads_update.html'
+    form_class = LeadModelForm
+    model = Lead
+
+    def get_success_url(self) -> str:
+        return reverse('leads:leads_details', kwargs={"pk": self.object.id})
 
 def lead_update(request, pk):
     lead = get_object_or_404(Lead, id=pk)
@@ -86,11 +118,18 @@ def lead_update(request, pk):
     }
     return render(request, 'leads/leads_update.html', context)
 
+#####
+
+#####
+class LeadDeleteView(DeleteView):
+    template_name = 'leads/lead_delete.html'  # This template is needed for confirmation
+    model = Lead
+    success_url = reverse_lazy('leads:leads_list')
 
 def lead_delete(request, pk):
     lead = get_object_or_404(Lead, id=pk)
     lead.delete()
     return redirect('leads:leads_list')
-
+#####
 
 
